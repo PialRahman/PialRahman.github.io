5952732093ba531ac6d6654b2cdbea90d146b666
PialRahman <58075858+PialRahman@users.noreply.github.com>
2020-02-16 19:14:45 +0600

    latest file

---
+++ Webtechassignment.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>pial rahman|pialrahman.github.io</title>
+</head>
+<body>
+	<form action="" method="POST">
+	<a href="http://pialrahman.github.io">pialrahman.github.io</a>
+	<hr>
+	<h1>Md. Zimmatur Rahman</h1>
+	<hr>
+	 Hello World,</p>
+	<p>This is Md. Zimmatur Rahman who is afresh graduate in CSE from AIUB,Bangladesh who has good knowledge about OOP and HTML and also developed project which is web based during graduation.</p><br>
+	<hr>
+	<h2>Education:</h2>
+	<ol>
+		<li>SSC<a href="http://www.ctgcs.edu.bd/">(Chattogram Collegiate School)</a></li>
+		<li>HSC<a href="https://www.abdurroufcollege.ac.bd/">(Birshreshtha Munshi Abdur Rouf Public College)</a></li>
+<li>Bsc<a href="https://www.aiub.edu/">(AIUB)</a></li>
+		
+	</ol>
+	<h2>Developed Projects Fields:</h2>
+	<ol>
+		<li><a href="http://www.cplusplus.com/doc/tutorial/">C++</a></li>
+		<li><a href="https://www.java.com/en/">Java</a></li>
+<li><a href="https://www.w3schools.com/html/">HTML</a></li>
+<li><a href="https://www.w3schools.com/php/">PHP</a></li>
+		
+	</ol>
+	</ol>
+	<h2>Soft Skills:</h2>
+	<ol>
+		<li>Communication</li>
+		<li>Group Work</li>
+       
+	</ol>
+</form>
+
+</body>
+</html>
